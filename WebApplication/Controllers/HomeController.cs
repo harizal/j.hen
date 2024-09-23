e67d434c6bec13769165ba5124d665efdc0c9982
hhi <hhi@voxteneo.com>
2024-09-23 17:55:54 +0700

    latest

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Linq;
 using WApp.Datas;
 using WApp.Helpers;
 using WApp.Models;
@@ -86,14 +87,14 @@ namespace WApp.Controllers
             return View(viewModel);
         }
 
-        public IActionResult Search()
+        public IActionResult Search(string name, string nik)
         {
             PegawaiViewModel model = new()
             {
                 ListJenisKelamin =
                 [
-                    new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem { Text = EnumHelper.GetDescription(Enums.JenisKelamin.L), Value = ((int)Enums.JenisKelamin.L).ToString() },
-                    new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem { Text = EnumHelper.GetDescription(Enums.JenisKelamin.P), Value = ((int)Enums.JenisKelamin.P).ToString() }
+                    new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem { Text = EnumHelper.GetDescription(Enums.JenisKelamin.L), Value = (Enums.JenisKelamin.L).ToString() },
+                    new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem { Text = EnumHelper.GetDescription(Enums.JenisKelamin.P), Value = (Enums.JenisKelamin.P).ToString() }
                 ],
                 ListStatusPerkawinan =
                 [
@@ -134,7 +135,10 @@ namespace WApp.Controllers
                         Text = m.Name,
                         Value = m.Id
                     }),
-                ]
+                ],
+
+                Name = string.IsNullOrEmpty(name) ? string.Empty : name,
+                NIK = string.IsNullOrEmpty(nik) ? string.Empty : nik,
             };
             return View(model);
         }
@@ -170,10 +174,15 @@ namespace WApp.Controllers
                                          Nomor = pegawai.Type != PegawaiType.PHL ? pegawai.NIK : string.Empty,
                                          NIK = pegawai.Type == PegawaiType.PHL ? pegawai.NIK : string.Empty,
                                          JenisKelaminText = EnumHelper.GetDescription(pegawai.JenisKelamin),
+                                         JenisKelamin = pegawai.JenisKelamin,
                                          PendidikanText = pen.Name ?? string.Empty,
+                                         PendidikanID = pen.Id ?? string.Empty,
                                          ProdiText = prod.Name ?? string.Empty,
+                                         ProdiID = prod.Id ?? string.Empty,
                                          SatuanKerjaText = satuanKerja.Name ?? string.Empty,
+                                         SatuanKerjaID = satuanKerja.Id ?? string.Empty,
                                          UnitKerjaText = unitKerja.Name ?? string.Empty,
+                                         UnitKerjaID = unitKerja.Id ?? string.Empty,
                                          TanggalAwal = ConverterHelper.DateTimeToString(pegawai.TanggalAwal),
                                          TanggalAkhir = ConverterHelper.DateTimeToString(pegawai.TanggalAkhir),
                                          Keterangan = pegawai.Keterangan,
@@ -183,8 +192,58 @@ namespace WApp.Controllers
                                          CreatedDate = pegawai.CreatedDate,
                                          UpdatedBy = pegawai.UpdatedBy,
                                          UpdatedDate = pegawai.UpdatedDate,
-                                         TypeText = EnumHelper.GetDescription(pegawai.Type)
+                                         TypeText = EnumHelper.GetDescription(pegawai.Type),
+                                         Type = pegawai.Type
                                      }).OrderByDescending(m => m.IsActive).ThenBy(m => m.Name).ToListAsync();
+
+                    if (filters.Count() > 1 && filters[2] != null && filters[2] != "null")
+                    {
+                        var jenisKelamin = filters[2]?.Split(',');
+                        if (jenisKelamin.Any())
+                            results = results.Where(m => jenisKelamin.Contains(m.JenisKelamin.ToString())).ToList();
+                    }
+
+                    if (filters.Count() > 2 && filters[3] != null && filters[3] != "null")
+                    {
+                        var pendidikan = filters[3]?.Split(',');
+                        if (pendidikan.Any())
+                            results = results.Where(m => pendidikan.Contains(m.PendidikanID.ToString())).ToList();
+                    }
+
+                    if (filters.Count() > 3 && filters[4] != null && filters[4] != "null")
+                    {
+                        var prodi = filters[4]?.Split(',');
+                        if (prodi.Any())
+                            results = results.Where(m => prodi.Contains(m.ProdiID.ToString())).ToList();
+                    }
+
+                    if (filters.Count() > 4 && filters[5] != null && filters[5] != "null")
+                    {
+                        var satuanKerja = filters[5]?.Split(',');
+                        if (satuanKerja.Any())
+                            results = results.Where(m => satuanKerja.Contains(m.SatuanKerjaID.ToString())).ToList();
+                    }
+
+                    if (filters.Count() > 5 && filters[6] != null && filters[6] != "null")
+                    {
+                        var unitKerja = filters[6]?.Split(',');
+                        if (unitKerja.Any())
+                            results = results.Where(m => unitKerja.Contains(m.UnitKerjaID.ToString())).ToList();
+                    }
+
+                    if (filters.Count() > 6 && filters[7] != null && filters[7] != "null")
+                    {
+                        var type = filters[7]?.Split(',');
+                        if (type.Any())
+                            results = results.Where(m => type.Contains(m.Type.ToString())).ToList();
+                    }
+
+                    if (filters.Count() > 7 && filters[8] != null && filters[8] != "null")
+                    {
+                        var status = filters[8]?.Split(',');
+                        if (status.Any())
+                            results = results.Where(m => status.Contains(m.IsActive ? "1" : "0")).ToList();
+                    }
                 }
                 catch (Exception ex)
                 {
